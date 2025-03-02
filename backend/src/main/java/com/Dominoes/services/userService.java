d1ccdd912954d4851975ebb6971abcb59a7ac2b6
bashar herzallah <basharherzallahua@gmail.com>
2025-03-02 22:16:11 +0000

    just learning some methods

--- backend/src/main/java/com/Dominoes/services/userService.java
+++ backend/src/main/java/com/Dominoes/services/userService.java
@@ -1,4 +1,14 @@
 package com.Dominoes.services;
+import com.Dominoes.module.user;
+
 
 public class userService {
+
+    public user user;
+
+    public userService(user user){
+        this.user = user;
+    }
+
+
 }
