bd1624a23841d4b0442b8e305e1a2010e4214852
INagy <i.nagy80@yahoo.com>
2025-03-08 02:46:09 +0100

    deleted the duplicate methods in IprofileDAO.java

--- backend/src/main/java/com/Dominoes/DAO/IprofileDAO.java
+++ backend/src/main/java/com/Dominoes/DAO/IprofileDAO.java
@@ -1,16 +1,12 @@
 package com.Dominoes.DAO;
 
 import com.Dominoes.model.profile;
-import com.Dominoes.model.user;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IprofileDAO {
-    Optional<profile> getprofileById(Integer id);
-
-    Optional<profile> getAllProfiles();
-
+    List<profile> getAllProfiles();
     Optional<profile> getProfileById(Integer id);
-    Optional<profile> getAllprofiles();
-    Optional<profile> getAllFriends();
+
 }
