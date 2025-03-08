777383b1d47dd13abecc1763c3d43dafbccf539c
INagy <i.nagy80@yahoo.com>
2025-03-08 02:59:25 +0100

    adjusted the userService class

--- backend/src/main/java/com/Dominoes/services/userService.java
+++ backend/src/main/java/com/Dominoes/services/userService.java
@@ -1,8 +1,12 @@
 package com.Dominoes.services;
-import com.Dominoes.DAO.IuserJPA;
+
 import com.Dominoes.DAO.userDAO;
-import com.Dominoes.model.user;
+import com.Dominoes.exception.duplicatResourceException;
+import com.Dominoes.exception.userNotFoundException;
+import com.Dominoes.model.mUser;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class userService {
@@ -14,8 +18,31 @@ public class userService {
     public userService(userDAO userDAO) {
         this.userDAO = userDAO;
     }
-    public user getUserById(Integer id){
-        return userDAO.getUserById(id).orElse(null);
+
+    public List<mUser> getAllUsers() {
+        return userDAO.getAllUsers();
+
     }
 
+    public mUser getUser(Integer id) {
+        return userDAO.getUserById(id).orElseThrow(
+                () -> new userNotFoundException("This user does not exist"));
+    }
+
+    public void addUser(mUser user) {
+        //check if email or username already exists
+        if (userDAO.existsUserByEmail(user.getEmail())) {
+            throw new duplicatResourceException("This email already exists");
+        } else if (userDAO.existsUserByUsername(user.getUsername())) {
+            throw new duplicatResourceException("This username already exists");
+
+            //add
+        }else{
+
+            userDAO.insertUser(user);
+
+
+        }
+
+    }
 }
