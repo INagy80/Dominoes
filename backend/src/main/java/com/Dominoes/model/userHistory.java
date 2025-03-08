4cb6441e129ac383155ad62220ea91a99edd5fb3
INagy <i.nagy80@yahoo.com>
2025-03-08 02:34:19 +0100

    in the userHistory.java the implementation was not quit right ther was some messing important things such as data relationships

--- backend/src/main/java/com/Dominoes/model/userHistory.java
+++ backend/src/main/java/com/Dominoes/model/userHistory.java
@@ -1,9 +1,82 @@
 package com.Dominoes.model;
+import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
+@Entity
 public class userHistory {
 
+
+    @Id
+    @SequenceGenerator(
+            name = "userHistory_id_sequence",
+            sequenceName = "userHistory_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "userHistory_id_sequence"
+
+    )
     private Integer id;
-    
+
+    @OneToMany (cascade = CascadeType.DETACH)
+    private List<game> wonGame;
+
+    @OneToMany(cascade = CascadeType.DETACH)
+    private List<game> lostGame;
+
+
+
+    public userHistory() {}
+
+    public userHistory(List<game> wonGame, List<game> lostGame) {
+        this.wonGame = wonGame;
+        this.lostGame = lostGame;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<game> getWonGame() {
+        return wonGame;
+    }
+
+    public void setWonGame(List<game> wonGame) {
+        this.wonGame = wonGame;
+    }
+
+    public List<game> getLostGame() {
+        return lostGame;
+    }
+
+    public void setLostGame(List<game> lostGame) {
+        this.lostGame = lostGame;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        userHistory that = (userHistory) o;
+        return Objects.equals(id, that.id) && Objects.equals(wonGame, that.wonGame) && Objects.equals(lostGame, that.lostGame);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, wonGame, lostGame);
+    }
+
+    @Override
+    public String toString() {
+        return "userHistory{" +
+                "id=" + id +
+                ", wonGame=" + wonGame +
+                ", lostGame=" + lostGame +
+                '}';
+    }
 }
