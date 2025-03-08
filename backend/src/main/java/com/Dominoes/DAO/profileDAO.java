7135d198d9ff2b1dba0fecc5baee2a7319b787d1
INagy <i.nagy80@yahoo.com>
2025-03-08 02:50:55 +0100

    profileDAO.java

--- backend/src/main/java/com/Dominoes/DAO/profileDAO.java
+++ backend/src/main/java/com/Dominoes/DAO/profileDAO.java
@@ -3,44 +3,29 @@ package com.Dominoes.DAO;
 import com.Dominoes.model.profile;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class profileDAO implements IprofileDAO {
 
+    //JPA object als attribute
+    private final IprofileJPA   profileJPA;
 
-    // Objekt erstellt
-    private final IprofileJPA iprofileJPA;
+    //dependency injection constructor
+    public profileDAO( IprofileJPA profileJPA) {
+        this.profileJPA = profileJPA;
 
-    //Konstruktor
-    public profileDAO(IprofileJPA profileJPA) {
-        this.iprofileJPA = profileJPA;
     }
 
-    @Override
-    public Optional<profile> getprofileById(Integer id) {
-        return iprofileJPA.findById(id);
-    }
 
     @Override
-    public Optional<profile> getAllProfiles() {
-        return getAllProfiles();
+    public List<profile> getAllProfiles() {
+        return profileJPA.findAll();
     }
 
     @Override
     public Optional<profile> getProfileById(Integer id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<profile> getAllprofiles() {
-        return Optional.empty();
-    }
-
-
-    @Override
-    public Optional<profile> getAllFriends() {
-
-        return Optional.empty();
+        return profileJPA.findById(id);
     }
 }
